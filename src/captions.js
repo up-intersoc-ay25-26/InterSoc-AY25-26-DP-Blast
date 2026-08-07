94f0a8fe8843b005fde3f80a5bf6024de70bdb86
nikkanap <nenaputo@up.edu.ph>
2026-08-07 16:30:47 +0800

    Add caption of AY 2024-2025 in captions.js

--- src/captions.js
+++ src/captions.js
@@ -3,11 +3,36 @@ export const getCaption = (year_level, academic_year, stud_name) => {
     const yearLevelFormatted = year_level.includes('faup') ? year_level.toUpperCase() : year_level.replace('-y', ' Y');
     
     const captions = {
+        "2024-2025": `
+        $ 𝚙𝚠𝚍
+        /𝚑𝚘𝚖𝚎/𝚜𝚝𝚞𝚍𝚎𝚗𝚝/𝙺𝙾𝙼𝚂𝙰𝙸
+        $ 𝚖𝚔𝚍𝚒𝚛 𝙰𝚈𝟸𝟺-𝟸𝟻
+        $ 𝚌𝚍 𝙰𝚈𝟸𝟺-𝟸𝟻 
+
+        Eyyy 🤙 I’m in… 🥷💻 in a new academic year as a ${yearLevelFormatted} B.S. in Computer Science student of UP Tacloban! 
+
+        I’m ${stud_name}! Come along with me 🎵 and the butterflies 🦋 and bees 🐝 (figuratively 😜) as we wander through the evolution of analog and digital technologies that define the modern age and the future. 
+
+        ‘Di ko man kaya i-h4ck ang FB Chats ng jowa mo or ayusin ang printer mo (actually kaya naman kung pag-aralan 😝)— as an Iskolar ng Bayan, I’m ready to learn and apply the knowledge and skills in the field of computing in order to solve the challenges from within the four walls of my classroom to the societal problems lying beyond these walls. ✊ 
+
+        𝚃𝚢𝚙𝚎 𝚊 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚝𝚘 𝚙𝚊𝚍𝚊𝚢𝚘𝚗… 🤖💾 
+
+        $ 
+
+        #UPTacloban
+        #AY2425
+        #KOMSAI 
+
+        -
+
+        🎨 Nikka Naputo
+        ✍ Joenne Amancio
+        `,
         "2025-2026": `
         I didn’t choose the grind — the grind chose me.” 💔⛏️
         But hey, might as well make it legendary.
 
-        Hi! I’m ${stud_name}, a proud ${year_level} BS Computer Science student from UP Tacloban 💻✨
+        Hi! I’m ${stud_name}, a proud ${yearLevelFormatted} BS Computer Science student from UP Tacloban 💻✨
         This year, we’re not just coding — we’re crafting our future, one block at a time.
         Join us as we touch grass (and maybe a few creepers) 🌱💥,
         explore the mysterious biomes of academic life 🧭📚,
